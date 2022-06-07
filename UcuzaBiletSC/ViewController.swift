de1199dc4baaac4023c1f50e052b3ba0c302653d
berkangzgn <bgezgin@protonmail.com>
2022-06-08 01:25:36 +0300

    Packages added

--- UcuzaBiletSC/ViewController.swift
+++ UcuzaBiletSC/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextLabel: UITextField!
+    @IBOutlet weak var sifreTextLabel: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func girisYapButton(_ sender: Any) {
+        performSegue(withIdentifier: "toMainPage", sender: nil)
+    }
+    
+    @IBAction func sifremiUnuttumButton(_ sender: Any) {
+    }
+    
+    @IBAction func kayitOlButton(_ sender: Any) {
+        performSegue(withIdentifier: "toMainPage", sender: nil)
+    }
 }
-
