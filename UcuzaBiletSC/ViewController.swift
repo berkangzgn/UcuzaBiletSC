0fb3800ed8b9408c5aeb88caa6bc126c2511f219
berkangzgn <bgezgin@protonmail.com>
2022-06-07 21:17:35 +0300

    Initial Commit

---
+++ UcuzaBiletSC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UcuzaBiletSC
+//
+//  Created by Berkan Gezgin on 7.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
