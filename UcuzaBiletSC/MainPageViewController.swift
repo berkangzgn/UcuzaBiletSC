70a2d5594ecabb9f5e21cc90be5c5dd74bf3b76c
berkangzgn <bgezgin@protonmail.com>
2022-06-09 15:38:19 +0300

    cikisButton bug fixed

--- UcuzaBiletSC/MainPageViewController.swift
+++ UcuzaBiletSC/MainPageViewController.swift
@@ -9,13 +9,9 @@ import UIKit
 import Firebase
 
 class MainPageViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
     
     @IBAction func cikisButton(_ sender: Any) {
         do {
@@ -25,15 +21,4 @@ class MainPageViewController: UIViewController {
             print("Error!")
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
