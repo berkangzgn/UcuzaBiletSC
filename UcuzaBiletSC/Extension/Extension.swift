e88dacce10473e2721442a79fb7783dbf6e72c29
berkangzgn <bgezgin@protonmail.com>
2022-06-10 14:40:40 +0300

    URL extension added

---
+++ UcuzaBiletSC/Extension/Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Extension.swift
+//  UcuzaBiletSC
+//
+//  Created by Berkan Gezgin on 10.06.2022.
+//
+
+import Foundation
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.compactMap { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
